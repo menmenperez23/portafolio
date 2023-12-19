fd918311a31c0d113dd1110cce1cd679621c5df1
menmenperez23 <154345677+menmenperez23@users.noreply.github.com>
2023-12-19 21:16:06 +0100

    initial conmit

---
+++ je.js
@@ -0,0 +1,18 @@
+// Función para mostrar/ocultar el menú desplegable
+function toggleNavbar() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
+
+// Cerrar el menú desplegable cuando se hace clic fuera de él
+window.onclick = function(event) {
+    if (!event.target.matches('.icon')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
